2a182cfbdb73100e1f944f6aab5a6b7115b81b3d
loureirolino <loureirolino.lucas@gmail.com>
2022-03-21 04:14:41 -0300

    "start"

---
+++ app.py
@@ -0,0 +1,233 @@
+###
+#Packages Dash
+import dash
+import dash_bootstrap_components as dbc
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output, State
+
+# Others Packages
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np 
+import pandas as pd
+
+
+###
+# Data
+#df = pd.read_excel()
+#df = pd.read_csv()
+
+
+# Core Struture
+app = dash.Dash(
+    __name__, external_stylesheets = [dbc.themes.BOOTSTRAP],title = "Nome do App"
+)
+
+server = app.server
+
+
+###
+# Page Component - Sidebar
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.Button("Sidebar", outline=True, color="secondary", className="mr-1", id="btn_sidebar"),
+        dbc.NavItem(dbc.NavLink("Page 1", href="#")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("More pages", header=True),
+                dbc.DropdownMenuItem("Page 2", href="#"),
+                dbc.DropdownMenuItem("Page 3", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="More",
+        ),
+    ],
+    brand="Project Name",
+    brand_href="#",
+    color="dark",
+    dark=True,
+    fluid=True,
+)
+
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 62.5,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "height": "100%",
+    "z-index": 1,
+    "overflow-x": "hidden",
+    "transition": "all 0.5s",
+    "padding": "0.5rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+SIDEBAR_HIDEN = {
+    "position": "fixed",
+    "top": 62.5,
+    "left": "-16rem",
+    "bottom": 0,
+    "width": "16rem",
+    "height": "100%",
+    "z-index": 1,
+    "overflow-x": "hidden",
+    "transition": "all 0.5s",
+    "padding": "0rem 0rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "transition": "margin-left .5s",
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+
+sidebar = html.Div(
+    [
+        html.H2("Sidebar", className="display-4"),
+        html.Hr(),
+        html.P(
+            "A simple sidebar layout with navigation links", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Page 1", href="/page-1", id="page-1-link"),
+                dbc.NavLink("Page 2", href="/page-2", id="page-2-link"),
+                dbc.NavLink("Page 3", href="/page-3", id="page-3-link"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    id="sidebar",
+    style=SIDEBAR_STYLE,
+)
+
+###
+# Dash - Page Layout
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
+
+app.layout = html.Div(   
+    [
+        dcc.Store(id='side_click'),
+        dcc.Location(id="url"),
+        navbar,
+        sidebar,
+        dbc.Button("Primary", outline=True, color="primary", className="me-1"),
+        dbc.Button(
+            "Secondary", outline=True, color="secondary", className="me-1"
+        ),
+        dbc.Button("Success", outline=True, color="success", className="me-1"),
+        dbc.Button("Warning", outline=True, color="warning", className="me-1"),
+        dbc.Button("Danger", outline=True, color="danger", className="me-1"),
+        dbc.Button("Info", outline=True, color="info", className="me-1"),
+        dbc.Button("Light", outline=True, color="light", className="me-1"),
+        dbc.Button("Dark", outline=True, color="dark"),
+        content,
+    ],
+    
+
+)
+
+###
+# Callbacks
+@app.callback(
+    [
+        Output("sidebar", "style"),
+        Output("page-content", "style"),
+        Output("side_click", "data"),
+    ],
+
+    [Input("btn_sidebar", "n_clicks")],
+    [
+        State("side_click", "data"),
+    ]
+)
+def toggle_sidebar(n, nclick):
+    if n:
+        if nclick == "SHOW":
+            sidebar_style = SIDEBAR_HIDEN
+            content_style = CONTENT_STYLE
+            cur_nclick = "HIDDEN"
+        else:
+            sidebar_style = SIDEBAR_STYLE
+            content_style = CONTENT_STYLE
+            cur_nclick = "SHOW"
+    else:
+        sidebar_style = SIDEBAR_STYLE
+        content_style = CONTENT_STYLE
+        cur_nclick = 'SHOW'
+
+    return sidebar_style, content_style, cur_nclick
+
+# this callback uses the current pathname to set the active state of the
+# corresponding nav link to true, allowing users to tell see page they are on
+@app.callback(
+    [Output(f"page-{i}-link", "active") for i in range(1, 4)],
+    [Input("url", "pathname")],
+)
+def toggle_active_links(pathname):
+    if pathname == "/":
+        # Treat page 1 as the homepage / index
+        return True, False, False
+    return [pathname == f"/page-{i}" for i in range(1, 4)]
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname in ["/", "/page-1"]:
+        return [
+            
+                html.H1('Page 1'),
+                html.P("This is the content of page 1!")
+            
+        ]
+    elif pathname == "/page-2":
+        return [
+            
+                html.H1('Page 2'),
+                html.P("This is the content of page 2!")
+            
+        ]
+    elif pathname == "/page-3":
+        return [
+            
+                html.H1('Page 3'),
+                html.P("This is the content of page 3!")
+            
+        ]
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+    
+###
+# RUN
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8086)
